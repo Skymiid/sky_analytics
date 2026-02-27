ef1c6bae55cef79b65f4fc9b5edec0b4c8c170ae
midc004@yahoo.com <midc004@yahoo.com>
2026-02-27 13:44:47 +0000

    init

---
+++ models/example/stg__employee.sql
@@ -0,0 +1,17 @@
+with renamed as (
+  select
+    Id as employee_id,
+    firstName as first_name,
+    lastName as last_name,
+    primaryWorkEmail as email,
+    HireDate as hire_date,
+    RehireDate as rehire_date,
+    terminationdate as termination_date,
+    active_status
+  from
+    sky.source.employee_mock
+)
+select
+  *
+from
+  renamed
